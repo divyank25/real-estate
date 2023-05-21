d0bd30ddbd5150d2623402a49c7f374a698ec7f6
DIVYANK TRIPATHI <73693350+divyank25@users.noreply.github.com>
2023-05-21 10:35:07 +0530

    real estate

---
+++ GetStarted.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./GetStarted.css"
+
+const GetStarted = () => {
+  return (
+    <section className='g-wrapper'>
+        <div className='paddings innerWidth g-container'>
+            <div className='flexColCenter inner-container'>
+                <span className='primaryText'>Get Started with Homyz</span>
+                <span className='secondaryText'>
+                    Subscribe and find super attractive price quotes and offers 
+                    <br/>
+                    Find your residence soon
+                </span>
+                <button className='button'>
+                    <a href="mailto:divyanktripathi12@gmail.com"> Get Started</a>
+                </button>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default GetStarted
